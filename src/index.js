7b110996ee123795a5f535e90178cb9e20666d0c
yanyan155 <jm@tut.by>
2018-09-15 12:43:06 +0300

    rewrite code, stack logic add

--- src/index.js
+++ src/index.js
@@ -1,70 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
 
-  function findCloseBracket (str, i) {
-    var sameBracketsSymbolAndCount = []; 
-    if(i !=0) {
-      i--;
-    }
-    for (i; i<str.length; i++) {
-      var symbol = str.charAt(i);
-      var isFind = bracketsConfig.some(elem => elem[1] == symbol); 
-      var isSame = bracketsConfig.some(elem => elem[0] == symbol);
-      if(isFind && !isSame) {
-        var newString = str.slice(0, i+1);
-        return newString;
-      } else if(isFind && isSame) {
-        var find = sameBracketsSymbolAndCount.find(elem => elem[0] === symbol);
-        if(find != undefined) {
-          var count = find[1];
-          if(count === 1) {
-             var findIndex = sameBracketsSymbolAndCount.findIndex(elem => elem[0] === symbol);
-             sameBracketsSymbolAndCount[findIndex][1]++;
-             var newString = str.slice(0, i+1);
-             return newString;
-          }
-        }else if(find === undefined) {
-          sameBracketsSymbolAndCount.push([symbol, 1]);
-        }
-      }
-    } return str;
-  }
 
-  function isCorrectStr (string) {
-    var lastSymbol = string.charAt(string.length-1);
-    var prevSymbol = string.charAt(string.length-2);
-    var bracketsIndex = bracketsConfig.find(elem => elem[1] === lastSymbol);
-    if (bracketsIndex[0] === prevSymbol && bracketsIndex[1] === lastSymbol) {
-      return true;
-    }
-    return false;
-  }
-
-  function newCheckingStr (correctstring) {
-    var length = correctstring.length;
-    var cuttingStr = correctstring.slice(0, -2);
-    var newStr = cuttingStr + str.slice(length);
-    index = length-2;
-    return newStr;
-  }
   if (str.length%2 != 0) {
     return false;
   }
-  var str = str;
-  var index = 0;
 
-  while(str != "") {
-    var getstr = findCloseBracket(str, index);
-    var isCorrect = isCorrectStr(getstr);
-    if(isCorrect) {
-      str = newCheckingStr(getstr);
-      if(str === "") {
+  let stack = [];
+  for (let j = 0; j <str.length; j++) {
+
+    let symbol = str.charAt(j);
+    for(let i = 0; i< bracketsConfig.length; i++) {
+
+      let item = bracketsConfig[i];
+      if(symbol === item[0] && symbol === item[1]) {
+        if(stack.length > 0 && stack[stack.length - 1][0] === i) {
+          stack.pop();
+          break;
+        }
+        stack.push([i, 0]);
         break;
+        
+      } 
+
+      if(symbol === item[0]) {
+        stack.push([i, 0]);
+        break;
+      } else if(symbol === item[1]) {
+
+        let openBracket = stack[stack.length - 1];
+        if(stack.length > 0 && openBracket[0] === i) {
+
+            stack.pop();
+            break;
+        } 
+          return false;
       }
-    } else {
-      return false;
     }
   }
-  return true;
+  if(stack.length === 0) {
+    return true;
+  }
+  return false;
 }
 
 
